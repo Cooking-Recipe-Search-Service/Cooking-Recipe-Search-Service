77057b4d49e3342c0aa8526f5a998834aed0c039
esharunov <evgeniy.sharunov@orioninc.com>
2022-04-24 23:27:58 +0300

    Fix Cors

--- server/src/main/java/com/crss/crss/settings/CrssConfiguration.java
+++ server/src/main/java/com/crss/crss/settings/CrssConfiguration.java
@@ -1,8 +1,14 @@
 package com.crss.crss.settings;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 
 @Configuration
@@ -11,5 +17,16 @@ public class CrssConfiguration {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("http://localhost:3000","http://localhost:4200","http://localhost:8080")); // Provide list of origins if you want multiple origins
+        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+        config.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
