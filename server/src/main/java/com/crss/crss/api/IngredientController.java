5c858a685938cc1522a579f0594024ef14da6c33
esharunov <evgeniy.sharunov@orioninc.com>
2022-04-24 12:51:10 +0300

    CRSS-20 Fix cross origins

--- server/src/main/java/com/crss/crss/api/IngredientController.java
+++ server/src/main/java/com/crss/crss/api/IngredientController.java
@@ -3,8 +3,6 @@ package com.crss.crss.api;
 import com.crss.crss.dto.IngredientDto;
 import com.crss.crss.dto.IngredientDtoIn;
 import com.crss.crss.dto.IngredientSlimDto;
-import com.crss.crss.dto.RecipeDto;
-import com.crss.crss.dto.RecipeDtoIn;
 import com.crss.crss.mapper.DtoConverter;
 import com.crss.crss.services.IngredientService;
 import java.util.List;
@@ -16,7 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @RequestMapping("/api/ingredients")
@@ -27,19 +24,16 @@ public class IngredientController {
     private final IngredientService ingredientService;
 
     @GetMapping
-    @CrossOrigin(origins = "http://localhost:4200")
     public List<IngredientSlimDto> getIngredients() {
         return dtoConverter.simpleConvert(ingredientService.getAllIngredients(), IngredientSlimDto.class);
     }
 
     @GetMapping("/{id}")
-    @CrossOrigin(origins = "http://localhost:4200")
     public IngredientDto getIngredient(@PathVariable Long id) {
         return dtoConverter.getIngredientDto(ingredientService.getIngredientById(id));
     }
 
     @PostMapping
-    @CrossOrigin(origins = "http://localhost:4200")
     public IngredientDto createIngredient(@RequestBody IngredientDtoIn dto) {
         return dtoConverter.getIngredientDto(ingredientService.createIngredient(dto));
     }
