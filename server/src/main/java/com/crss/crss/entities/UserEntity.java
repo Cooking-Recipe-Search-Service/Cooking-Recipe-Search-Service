7a29286655478a51fa5acc71c86549bf4bf3a47a
evsharun <jenya.sharunov@gmail.com>
2022-05-03 23:38:45 +0300

    add auth

--- server/src/main/java/com/crss/crss/entities/UserEntity.java
+++ server/src/main/java/com/crss/crss/entities/UserEntity.java
@@ -10,28 +10,16 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "user",
-    uniqueConstraints = {
-        @UniqueConstraint(columnNames = "username"),
-        @UniqueConstraint(columnNames = "email")
-    })
+@Table(name = "user")
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class UserEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotBlank
-    @Size(max = 20)
     private String username;
-    @NotBlank
-    @Size(max = 50)
-    @Email
     private String email;
-    @NotBlank
-    @Size(max = 120)
     private String password;
     private UserRole role;
 
--- server/src/main/java/com/crss/crss/settings/SwaggerConfiguration.java
+++ server/src/main/java/com/crss/crss/settings/SwaggerConfiguration.java
@@ -1,15 +1,31 @@
 package com.crss.crss.settings;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "CRSS", description = "Cooking Recipe Search Service",
-    version = "1.0", contact = @Contact(name = "Sharunov Evgeniy", email = "easharunov@edu.hse.ru")))
 public class SwaggerConfiguration {
 
+    @Bean
+    public OpenAPI openApi() {
+        final String securitySchemeName = "bearerAuth";
+
+        return new OpenAPI().info(new Info().title("CRSS").description("Cooking Recipe Search Service").version("v1.0")
+                .contact(new Contact().name("Sharunov Evgeniy")))
+            .addSecurityItem(new SecurityRequirement().addList(securitySchemeName)).components(
+                new Components()
+                    .addSecuritySchemes(securitySchemeName,
+                        new SecurityScheme()
+                            .type(SecurityScheme.Type.HTTP)
+                            .scheme("bearer")
+                            .bearerFormat("JWT")));
+    }
 
 }
