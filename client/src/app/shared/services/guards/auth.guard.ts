f68132910efc2eb6562e5d3ee8387be9326d5c59
TatyanaKonina <tdkonina@edu.hse.ru>
2022-05-10 12:00:09 +0300

    fix: fix nav

--- client/src/app/shared/services/guards/auth.guard.ts
+++ client/src/app/shared/services/guards/auth.guard.ts
@@ -4,6 +4,7 @@ import { CanActivate, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
 import { LocalStorageService } from '../local-storage/local-storage.service';
+import { NotificationService } from '../notifications/notification.service';
 
 @Injectable({
     providedIn: 'root',
@@ -12,13 +13,15 @@ export class AuthGuard implements CanActivate {
     constructor(
         private router: Router,
         private localStorage: LocalStorageService,
+        private notificationService: NotificationService
     ) {}
 
     canActivate(): Observable<boolean> {
         return this.localStorage.getToken().pipe(
             map((token: string | null) => !!token),
             tap((token: boolean) => {
-                if (token === null) {
+                if (!token) {
+                    // this.notificationService.showNeedLoginNotification()
                     this.router.navigate(['/login']);
                 }
             }),
--- client/src/app/shared/services/notifications/notification/need-login-notification.module.ts
+++ client/src/app/shared/services/notifications/notification/need-login-notification.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NeedLoginNotificationComponent } from './need-login-notification.component';
 import { TuiButtonModule } from '@taiga-ui/core';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     declarations: [NeedLoginNotificationComponent],
-    imports: [CommonModule, TuiButtonModule],
+    imports: [CommonModule, TuiButtonModule,RouterModule],
     exports: [NeedLoginNotificationComponent],
 })
 export class NeedLoginNotificationModule {}
